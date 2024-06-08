57cce468bc91ac802ec5a7e456be968831733997
Roosvansambeek <123948541+Roosvansambeek@users.noreply.github.com>
2024-06-08 12:46:01 +0000

    navbar, footer, en nieuwe pagina's

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
   return render_template('home.html')
+
+@app.route("/overons")
+def overons():
+  return render_template('overons.html')
+
+@app.route("/werkzaamheden")
+def werkzaamheden():
+  return render_template('werkzaamheden.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
