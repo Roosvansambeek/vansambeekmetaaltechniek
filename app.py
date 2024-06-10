53272ff7321ec461586f2e7646c97984a00ce487
Roosvansambeek <123948541+Roosvansambeek@users.noreply.github.com>
2024-06-10 10:24:53 +0000

    email versturen met de aanvraag

--- app.py
+++ app.py
@@ -22,9 +22,39 @@ def contact():
 @app.route("/contact/verstuurd", methods=["POST"])
 def vraag_verstuurd():
     data = request.form
+    send_email(data)
     print(data)  # Log de ontvangen data
     return render_template('aanvraag.html', aanvraag=data)
 
+
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+def send_email(data):
+  email_sender = "sambeekvanroos@gmail.com"
+  email_password = 'gfdt unhv hrio clos'
+  email_receiver = "sambeekvanroos@gmail.com"
+  
+  subject = '(aan)vraag'
+  body = f"""
+    Naam: {data['full-name']}
+    Email: {data['email']}
+    Telefoonnummer: {data['telefoonnummer']}
+    Vraag/offerte aanvraag: {data['vraag']}
+    """
+  
+  em = EmailMessage()
+  em['From'] = email_sender
+  em['To'] = email_receiver
+  em['Subject'] = subject
+  em.set_content(body)
+  
+  context = ssl.create_default_context()
+  
+  with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
